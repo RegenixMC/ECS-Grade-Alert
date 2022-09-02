a1fea7d165aa90f747ba3a7c1ac02e27566b5d7d
Regenix <73912088+RegenixMC@users.noreply.github.com>
2022-09-02 19:21:52 -0400

    Add files via upload

--- databaseManager.py
+++ databaseManager.py
@@ -1,12 +1,40 @@
-def addUser(userName, loginEmail, loginPassword):
-    if ' ' in userName:
-        return 1
-    if ' ' in loginEmail:
-        return 1
-    if ' ' in loginPassword:
-        return 1
+import sys
+
+def addUser(username, email, password):
+    with open("database/user_database.json", "a") as database:
+        database.write(username + ' ' + email + ' ' + password + '\n')
+    return 2
 
     
-    
+while True:
+    while True:
+        code = 0
+        username = str(input('Enter FACTS username: '))
+        password = str(input('Enter FACTS password: '))
+        email = str(input('Enter email: '))
+        if ' ' in username:
+            username = username.replace(' ', '_')
+            code = 2
+        if ' ' in email:
+            print("Can't have spaces in the email!\n---------------------------------------------")
+            code = 1
+        if ' ' in password:
+            print("Can't have spaces in the password!\n---------------------------------------------")
+            code = 1
 
-#addUser('Derek', 'derek.m.britton@gmail.com', 'Spa#1295')
+        if code == 1:
+            pass
+        else:
+            break
+        
+        
+        
+
+        
+    userSubmit = addUser(username, email, password)
+    if userSubmit == 2:
+        print('User added!')
+    
+    if int(input('Type 1 to add a new user, type 2 to end program: ')) == 2:
+        break
+    print('---------------------------------------------')
---
+++ sendAllAlerts.py
@@ -0,0 +1,81 @@
+import requests
+from getAssignmentDate import getAssignmentDueDate
+from alert import *
+from databaseManager import addUser
+import sys
+from datetime import date
+from getCalendarDay import *
+
+
+
+def sendAllAlerts():
+
+    userDatabase = open('database/user_database.json', 'r')
+    for line in userDatabase:
+        username = str(line.split(' ')[0]).replace('_', ' ')
+        email = str(line.split(' ')[1].split(' ')[0])
+        password = (str(line.split(' ')[2]))
+        #print(username, email, password) # log the user information for testing purposes
+
+
+        payload = {
+            'UserType': 'PARENTSWEB-STUDENT',
+            'login': 'Log In',
+            'DistrictCode': 'EC-VA',
+            'username': username,
+            'password': password
+        }
+
+
+        with requests.Session() as s:
+            page = s.post('https://ec-va.client.renweb.com/pwr/', data=payload)
+            page = (s.get('https://ec-va.client.renweb.com/pwr/student/homework.cfm')).text
+
+
+            #if int(input('Type "1" if you would like to save the page html code to text file. Type "2" to cancel: ')) == 1:
+                #with open("PageHTML.txt", "w") as text_file:
+                    #text_file.write(page)
+
+
+            count = int(page.count('h3 class="pwr_card-heading alt">'))
+            count1=0
+            emailContextClasses=[]
+            userName = ((page.split('<div class="pwr_user-name">'))[1].split('</div>')[0])
+            classNames = '\n'
+
+            for i in range(count):
+                count1+=1
+                classUnsortedText = ((page.split('h3 class="pwr_card-heading alt"'))[count1].split('</div>')[0])
+
+                assignmentName = ((classUnsortedText.split('>'))[1].split('<')[0])
+                assignmentDueDate = getAssignmentDueDate(classUnsortedText, page)
+
+                currentYear = date.today().strftime("%Y")
+                currentMonth = (date.today().strftime("%m")).replace('0', '')
+                currentDay = (int(date.today().strftime("%d")) + 1)
+                tmr = str(str(currentMonth) + '/' + str(currentDay) + '/' + str(currentYear))
+
+                if tmr == assignmentDueDate:
+                    emailContextClasses.append(assignmentName)
+                #print(assignmentName + ' ' + assignmentDueDate)
+
+
+
+        emailContext = ('Good evening ' + userName + ',\n\n' + 'Tomorrow will be a ' + getDayColor(username, password) + ' Day so make make sure you have the required notebooks and folders for your classes.\n')
+        
+        if emailContextClasses != []:
+            classesDueTmr = int(len(emailContextClasses))
+            for i in range(classesDueTmr):
+                classNames = (classNames + emailContextClasses[i] + '\n')
+
+            homeworkStatus = False
+            emailContext = ('Good evening ' + userName + ',\n\n' + 'Tomorrow will be a ' + getDayColor(username, password) + ' Day so make make sure you have the required notebooks and folders.\n' + 'You have an assignment and/or a test due tomorrow in the following classes...' + classNames)
+
+        else:
+            homeworkStatus = True
+            emailContext = ('Good evening ' + userName + ',\n\n' + 'Tomorrow will be a ' + getDayColor(username, password) + ' Day so make make sure you have the required notebooks and folders.\n' + 'It\'s your lucky day! You have no assignments or tests due tomorrow!')
+
+
+
+
+        sendEmail(emailContext, email)
